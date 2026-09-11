2bdee9d5b4bc0e107c3b8cb3dfe38e1c1f4a69de
sachinsinghpatwal <sachinsingh71004@gmail.com>
2026-09-11 15:28:32 +0530

    final check

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -5,7 +5,7 @@ import connectToMongoDb from "./db/MongoDb.js";
 connectToMongoDb()
   .then(() => {
     const port = Number(process.env.PORT) || 8000;
-    app.listen(port, () => {
+    app.listen(port,"0.0.0.0", () => {
       console.log(`⚙️ Server is running at port : ${port}`);
     });
   })
