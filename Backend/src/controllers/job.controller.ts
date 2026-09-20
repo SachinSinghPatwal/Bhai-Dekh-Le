e259c401b5c9c6bb973215b5bcef5826ff7082c5
sachinsinghpatwal <sachinsingh71004@gmail.com>
2026-09-20 17:35:42 +0530

    refactor: clean up job controller and routes

--- Backend/src/controllers/job.controller.ts
+++ Backend/src/controllers/job.controller.ts
@@ -1,26 +1,11 @@
 import { AsyncHandler } from "../utility/AsyncHandler.js";
-import { JobModel } from "../models/Mongo/job.models.js";
-import AsyncHandlerTry_CatchWrapper from "../utility/AsyncHandlerContentWrapper.js";
 import type { Request, Response } from "express";
-
-import endpointRequestBodyValidation from "../utility/EndpointRequestBodyValidation.js";
 import ScheduleScrapping from "../services/RMQ/Producer/ScheduleScrape.js";
 import { ApiResponse } from "../utility/ApiResponse.js";
 
-// export const createJob = AsyncHandler(async (req: Request, res: Response) => {
-//   const validatedData = endpointRequestBodyValidation(req as Request);
-
-//   const job = await AsyncHandlerTry_CatchWrapper(async () => {
-//     return JobModel.create();
-//   }, validatedData);
-
-//   res.status(201).json({
-//     success: true,
-//     job,
-//   });
-// });
 
 export const getAllJobs = AsyncHandler(async (_: Request, res: Response) => {
+
   await ScheduleScrapping();
 
   return res.status(202).json(
--- Backend/src/routes/job.routes.ts
+++ Backend/src/routes/job.routes.ts
@@ -1,12 +1,9 @@
 import {Router} from "express";
-import { createJob, getAllJobs, getJobById, updateJob, deleteJob } from "../controllers/job.controller.js";
+import {getAllJobs} from "../controllers/job.controller.js";
 
 const router = Router();
 
-router.post("/create", createJob);
 router.get("/getAll", getAllJobs);
-router.get("/:id", getJobById);
-router.put("/:id", updateJob);
-router.delete("/:id", deleteJob);
+
 
 export default router;
